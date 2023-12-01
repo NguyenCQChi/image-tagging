0b5acfec03d3f823a8013c9b7792546849aa7c64
Chi Nguyen <chinsu190103@gmail.com>
2023-12-01 02:18:34 -0800

    Fix url

--- src/containers/Admin/Admin.tsx
+++ src/containers/Admin/Admin.tsx
@@ -49,7 +49,7 @@ const Admin = () => {
   useEffect(() => {
     console.log('trying to get userList')
     userList.forEach(user => {
-      const server_url = `${API_IMAGE_SERVER}${API_USER_STAT}?userId=${user.userName}`
+      const server_url = `${API_IMAGE_SERVER}${API_USER_STAT}?userID=${user.userName}`
       const apiResponse = api.get(server_url);
 
       apiResponse.then((response) => {
